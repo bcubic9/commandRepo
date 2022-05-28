f8047f1e84a6bb90c197b41a532a3681bb1d9bc6
Rprentice20 <70124893+Rprentice20@users.noreply.github.com>
2022-05-28 17:28:58 -0400

    Update FiveMinCommand.java

--- command/FiveMinCommand.java
+++ command/FiveMinCommand.java
@@ -5,18 +5,9 @@ import java.util.concurrent.TimeUnit;
 public class FiveMinCommand implements Command {
     
     public FiveMinCommand(StopWatch watch) {
-
+      this.watch = watch;
     }
 
-    public void execute() 
-    {
-        try
-        {
-
-        }
-        catch(Exception e)
-        {
-            
-        }
+    public void execute() {
+        watch.countDownFiveMin();
     }
-}
