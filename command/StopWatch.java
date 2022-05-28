0391cc12f22cd940d7ab70c8b33a3ac5ec8dff2f
Rprentice20 <70124893+Rprentice20@users.noreply.github.com>
2022-05-28 17:32:17 -0400

    Update StopWatch.java

--- command/StopWatch.java
+++ command/StopWatch.java
@@ -1,20 +1,40 @@
 package command;
 
 public class StopWatch {
-    
-    public StopWatch() {
+  public StopWatch() {
+  }
 
+  public void countDownOneMin() {
+    countdownFromInt(59);
+  }
+
+  public void countDownThirtySec() {
+    countdownFromInt(29);
+  }
+
+  public void countDownFiveMin() {
+    countdownFromInt(299);
+  }
+
+  private void countdownFromInt(int seconds) {
+    for (int i = seconds; i >= 0; i--) {
+      String valOfI = (i >= 10) ? String.valueOf(i) : "0" + String.valueOf(i);
+      System.out.println("00:" + valOfI);
+      this.sleep();
+      this.clearConsole();
     }
+  }
 
-    public void countDownOneMin() {
-        
+  private void clearConsole() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  } 
+  
+  private void sleep() {
+    try {
+      TimeUnit.MILLISECONDS.sleep(100);
+    } catch(Exception e) {
+            
     }
-
-    public void countDownFiveMin() {
-
-    }
-
-    public void countDownThirtySec() {
-
-    }
+  }
 }
