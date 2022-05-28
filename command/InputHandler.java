7e1ab9e98d5dceccd8181e6b6ebd4499be707c52
bcubic9 <bcubic@email.sc.edu>
2022-05-28 14:15:37 -0400

    Fixed package issue and edited inputEntered

--- command/InputHandler.java
+++ command/InputHandler.java
@@ -2,15 +2,24 @@ package command;
 
 import java.util.HashMap;
 
-public class InputHandler  {
-    
+public class InputHandler  
+{
     private HashMap<String, Command> commands = new HashMap<String, Command>();
-
-    public InputHandler(StopWatch watch) {
-
+    public InputHandler(StopWatch watch) 
+    {
+        
     }
 
-    public boolean inputEntered(String data) {
-        return false;
+    public boolean inputEntered(String data) 
+    {
+        if(commands.containsKey(data))
+        {
+            commands.get(data).execute();
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 }
