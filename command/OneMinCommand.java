655ed97bc760402afd5161efa342c6c0024357bb
AaronK <alkeys89@gmail.com>
2022-05-28 19:11:30 -0400

    fixing more errors

--- command/OneMinCommand.java
+++ command/OneMinCommand.java
@@ -5,10 +5,11 @@ import java.util.concurrent.TimeUnit;
 public class OneMinCommand implements Command {
     StopWatch watch = new StopWatch();
 
-    public OneMinCommand(StopWatch watch) 
+    public OneMinCommand(StopWatch watch) {
         this.watch = watch;
     }
 
     public void execute() {
       watch.countDownOneMin();
     }
+}
